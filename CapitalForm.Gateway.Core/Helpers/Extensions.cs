5c2f51db6f6b7b3a26a07e36e2d521e853b55642
david-ae <davidedet47@gmail.com>
2024-06-24 16:08:48 +0100

    Added Gateway Sample API

---
+++ CapitalForm.Gateway.Core/Helpers/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+
+namespace CapitalForm.Gateway.Core.Helpers
+{
+    public static class Extensions
+    {
+        public static string GetUrl(string microservice, string controller, string method, object? fromQueryParamenters = null)
+            => $"{microservice}api/{controller}/{method}{ToQueryString(fromQueryParamenters)}";
+
+        public static string ToQueryString(object? queryParameters)
+        {
+            if (queryParameters == null) return "";
+
+            List<string> queryParams = new() { };
+
+            foreach(var prop in queryParameters.GetType().GetProperties())
+            {
+                string? value = prop.GetValue(queryParameters, null)?.ToString();
+                if(value != null)
+                {
+                    queryParams.Add($"{HttpUtility.UrlEncode(prop.Name)}={HttpUtility.UrlEncode(value)}");
+                }
+            }
+
+            return string.Concat("?", string.Join("&", queryParams));
+        }
+    }
+}
---
+++ CapitalForm.Gateway.Core/Http/FormHttpClient.cs
@@ -0,0 +1,81 @@
+﻿using CapitalForm.Gateway.Core.Helpers;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CapitalForm.Gateway.Core.Http
+{
+    public class FormHttpClient
+    {
+        public HttpClient Client { get; }
+
+        public FormHttpClient(HttpClient httpClient)
+        {
+            Client = httpClient;
+        }
+
+        public virtual async Task<TOut> MakeRequestAsync<T, TOut>(string relativeAddress, HttpMethod method, T? postBody) where T : class
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                //RequestUri = new Uri(relativeAddress),
+                RequestUri = new Uri(Client.BaseAddress + relativeAddress),
+                Content = new StringContent(JsonConvert.SerializeObject(postBody == null ? (object)string.Empty : postBody), Encoding.UTF8, "application/json")
+            };
+
+            request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
+            return await Send<TOut>(request);
+        }
+
+        public virtual Task<TOut> MakeRequestAsync<T, TOut>(string microservice, string controller, string methodName, HttpMethod method, object? fromQueryParameters = null, T? postBody = null) where T : class
+            => MakeRequestAsync<T, TOut?>(relativeAddress: Extensions.GetUrl(microservice, controller, methodName, fromQueryParameters), method: method, postBody: postBody);
+
+        private async Task<T?> Send<T>(HttpRequestMessage request)
+        {
+            var response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            if(!response.IsSuccessStatusCode)
+            {
+                var ex = await GetException(request, response);
+                throw ex;
+            }
+
+            await using var stream = await response.Content.ReadAsStreamAsync();
+            using var streamReader = new StreamReader(stream);
+            using var jsonReader = new JsonTextReader(streamReader);
+
+            var result = new JsonSerializer()
+                .Deserialize<T>(jsonReader);
+
+            return result;
+        }
+
+        private static async Task<HttpRequestException> GetException(HttpRequestMessage request, HttpResponseMessage response)
+        {
+            request.Headers.TryGetValues("Connection", out var headers);
+            var content = response.Content != null ? await response.Content.ReadAsStringAsync() : null;
+
+            var url = request?.RequestUri?.ToString();
+            var messageResult = new MessageResult();
+
+            if(!string.IsNullOrEmpty(content) && content.StartsWith("{"))
+            {
+                messageResult = JsonConvert.DeserializeObject<MessageResult>(content);
+            }
+            else
+            {
+                messageResult.Message = content ?? "";
+            }
+
+            var ex = new HttpRequestException(messageResult?.Message, null, response.StatusCode);
+
+            return ex;
+        }
+    }
+
+    public class MessageResult
+    {
+        public string Message { get; set; } = string.Empty;
+        public bool isSuccess { get; set; } = false;
+    }
+}
