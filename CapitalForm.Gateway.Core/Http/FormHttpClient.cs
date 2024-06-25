f2f426364ae43591b78e925580ff5094f8fa0e9c
david-ae <davidedet47@gmail.com>
2024-06-25 16:24:41 +0100

    FInished HttpClient wrapper

--- CapitalForm.Gateway.Core/Http/FormHttpClient.cs
+++ CapitalForm.Gateway.Core/Http/FormHttpClient.cs
@@ -1,4 +1,5 @@
 ﻿using CapitalForm.Gateway.Core.Helpers;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -13,7 +14,7 @@ namespace CapitalForm.Gateway.Core.Http
             Client = httpClient;
         }
 
-        public virtual async Task<TOut> MakeRequestAsync<T, TOut>(string relativeAddress, HttpMethod method, T? postBody) where T : class
+        public virtual async Task<TOut?> MakeRequestAsync<T, TOut>(string relativeAddress, HttpMethod method, T? postBody) where T : class
         {
             var request = new HttpRequestMessage
             {
@@ -26,9 +27,63 @@ namespace CapitalForm.Gateway.Core.Http
             request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
             return await Send<TOut>(request);
         }
+        
+        public virtual async Task<FileResult> MakeFileRequestAsync<T, TOut>(string relativeAddress, HttpMethod method, T? postBody) where T : class
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = new Uri(Client.BaseAddress + relativeAddress),
+                Content = new StringContent(string.Empty, Encoding.UTF8, "application/json")
+            };
 
-        public virtual Task<TOut> MakeRequestAsync<T, TOut>(string microservice, string controller, string methodName, HttpMethod method, object? fromQueryParameters = null, T? postBody = null) where T : class
+            request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
+            return await Send<TOut>(request);
+        }
+
+        public virtual async Task<TOut?> MakeRequestRelativeAsync<T, TOut>(string relativeAddress, HttpMethod method, T? postBody) where T : class
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = new Uri(relativeAddress),
+                Content = new StringContent(JsonConvert.SerializeObject(postBody), Encoding.UTF8, "application/json")
+            };
+
+            request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
+            return await Send<TOut>(request);
+        }
+
+        public virtual Task<TOut?> MakeRequestAsync<T, TOut>(string microservice, string controller, string methodName, HttpMethod method, object? fromQueryParameters = null, T? postBody = null) where T : class
             => MakeRequestAsync<T, TOut?>(relativeAddress: Extensions.GetUrl(microservice, controller, methodName, fromQueryParameters), method: method, postBody: postBody);
+
+        public virtual async Task<TOut?> MakeMultiFormRequestAsync<T, TOut>(string relativeAddress, HttpMethod method, MultipartFormDataContent? postBody = null) where T : class
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = new Uri(Client.BaseAddress + relativeAddress),
+                Content = postBody
+            };
+
+            request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
+            return await Send<TOut?>(request);
+        }
+
+        public async Task<byte[]> MakeRequestForFileStream<T>(string relativeAddress, HttpMethod method, T? postBody = null) where T : class
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = new Uri(Client.BaseAddress + relativeAddress),
+                Content = new StringContent(JsonConvert.SerializeObject(postBody ?? (object)string.Empty), Encoding.UTF8, "application/json")
+            };
+
+            request.Headers.Authorization = Client.DefaultRequestHeaders.Authorization;
+            return await SendRequest<byte[]>(request);
+        }
+
+        #region Private Methods
 
         private async Task<T?> Send<T>(HttpRequestMessage request)
         {
@@ -46,6 +101,42 @@ namespace CapitalForm.Gateway.Core.Http
 
             var result = new JsonSerializer()
                 .Deserialize<T>(jsonReader);
+
+            return result;
+        }
+        
+        private async Task<byte[]> SendRequest<T>(HttpRequestMessage request)
+        {
+            var response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            if(!response.IsSuccessStatusCode)
+            {
+                var ex = await GetException(request, response);
+                throw ex;
+            }
+
+            var stream = await response.Content.ReadAsByteArrayAsync();
+
+            var result = stream;
+
+            return result;
+        }
+
+        private async Task<FileResult> Download(HttpRequestMessage request)
+        {
+            var response = await Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+            if(!response.IsSuccessStatusCode)
+            {
+                var ex = await GetException(request, response);
+                throw ex;
+            }
+
+            var stream = new MemoryStream();
+            (await response.Content.ReadAsStreamAsync()).CopyTo(stream);
+
+            var result = new FileContentResult(stream.ToArray(), "application/octet-stream");
+            result.FileDownloadName = response.Content.Headers.ContentDisposition?.FileName;
 
             return result;
         }
@@ -71,6 +162,7 @@ namespace CapitalForm.Gateway.Core.Http
 
             return ex;
         }
+        #endregion
     }
 
     public class MessageResult
---
+++ CapitalForm.Gateway.Core/Http/TemplateHttpClient.cs
@@ -0,0 +1,12 @@
+﻿namespace CapitalForm.Gateway.Core.Http
+{
+    public class TemplateHttpClient : FormHttpClient
+    {
+        private HttpClient _HttpClient;
+
+        public TemplateHttpClient(HttpClient httpClient): base(httpClient)
+        {
+            _HttpClient = httpClient;
+        }
+    }
+}
